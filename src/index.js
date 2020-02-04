895b7856b87f95d08cdda588b7cdf071adec3913
toda93 <dttong93@gmail.com>
2020-02-04 13:00:10 +0700

    sync

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-import { scheduleUnique } from './schedule';
+const schedule = require('node-schedule');
 
-export default scheduleUnique;
+let listJob = new Set();
+function scheduleUnique(name, rule, timeout, callback) {
+    const currentJob = schedule.scheduledJobs[name];
+    if (!currentJob) {
+        schedule.scheduleJob(name, rule, () => {
+            try {
+                if (!listJob.has(name)) {
+                    listJob.add(name);
+                    Promise.race([
+                        delay(timeout),
+                        callback()
+                    ]).then(result => {
+                        listJob.delete(name);
+                        if (result === 'timeout') {
+                            process.kill(process.pid);
+                        }
+                    });
+                }
+            } catch (e) {
+                console.error('error', e);
+                process.exit(1);
+            }
+        });
+    } else {
+        throw `duplicate job ${name}`;
+    }
+}
+
+function delay(timeout) {
+    return new Promise((resolve, reject) => {
+        let wait = setTimeout(() => {
+            clearTimeout(wait);
+            resolve('timeout')
+        }, timeout * 1000)
+    });
+}
+
+module.exports = scheduleUnique;
--- src/test.js
+++
@@ -1,5 +0,0 @@
-import { scheduleUnique } from './schedule';
-
-scheduleUnique('test', '* * * * *', 60 * 60, async () => {
-    console.log('test');
-});
